f4472f69cfc9d3bf6f52a3e61bc401d5d524d1e6
Hichan Shin <hichan0310@kaist.ac.kr>
2024-12-28 16:10:37 +0900

    damage display 하는중

--- Assets/GameBackend/Entity.cs
+++ Assets/GameBackend/Entity.cs
@@ -1,13 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using GameBackend.Events;
+using GameBackend.Status;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 namespace GameBackend
 {
-    public class Entity : MonoBehaviour
+    public abstract class Entity : MonoBehaviour
     {
+        public float speed { get; set; }
         public PlayerStatus status { get; set; }
         public List<IEntityEventListener> eventListener { get; set; }
+
+        public Entity()
+        {
+            speed = 1;
+            status = new PlayerStatus(1, 0, 0);
+            eventListener = new List<IEntityEventListener>();
+        }
 
         public void addListener(IEntityEventListener listener)
         {
@@ -19,9 +31,29 @@ namespace GameBackend
             this.eventListener.Remove(listener);
         }
 
-        private void cleanEventListener()
+        protected virtual void eventActive(EventArgs e)
         {
-            eventListener.RemoveAll(listener => listener == null);
+            foreach (var listener in eventListener)
+            {
+                listener.eventActive(e);
+            }
+        }
+
+        protected abstract void update(float deltaTime);
+
+        public void Update()
+        {
+            update(Time.deltaTime);
+        }
+
+        public void dmgtake(DmgGiveEvent dmg)
+        {
+            int def = status.def;
+            int C = 200;
+            int realDmg = (int)(dmg.trueDmg * ((float)(def) / (def + C)));
+            
+            eventActive(new DmgTakeEvent(realDmg, dmg.attacker, dmg.target, dmg.atkTags));
+            status.nowHp -= realDmg;
         }
     }
 }
