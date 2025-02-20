91dcf9fe5ccf8d10a138386e748078b82860389a
hichan <maxmath0310@gmail.com>
2025-02-21 00:03:32 +0900

    progressbar

---
+++ Assets/GameBackend/Objects/ProgressBar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace GameBackend.Objects
+{
+    public class ProgressBar:MonoBehaviour
+    {
+        public PlayerObject player;
+        private GameObject progressBar;
+        private float length = 1.28f;
+
+        private void Start()
+        {
+            progressBar = GameObject.Find("progressbar");
+        }
+
+        private void Update()
+        {
+            float ratio = 1-player.normalSkill.timeleft / 5;
+            var scale = progressBar.transform.localScale;
+            scale.x = ratio;
+            progressBar.transform.localScale = scale;
+            progressBar.transform.localPosition = new Vector3(length*ratio/2-length/2,0,2);
+        }
+    }
+}
