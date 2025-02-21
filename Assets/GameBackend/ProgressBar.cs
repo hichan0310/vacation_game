3bd430db51303b3cf537774346d7ead98fd11c73
hichan <maxmath0310@gmail.com>
2025-02-21 15:46:19 +0900

    asdf

--- Assets/GameBackend/ProgressBar.cs
+++ Assets/GameBackend/ProgressBar.cs
@@ -7,10 +7,12 @@ namespace GameBackend
         private GameObject progressBar;
         private float length = 1.28f;
         public float ratio=0;
+        private Vector3 startPosition;
 
         private void Start()
         {
-            progressBar = GameObject.Find("progressbar");
+            progressBar = GameObject.Find("progressbar"); 
+            startPosition = progressBar.transform.localPosition;
         }
 
         private void Update()
@@ -18,7 +20,7 @@ namespace GameBackend
             var scale = progressBar.transform.localScale;
             scale.x = ratio;
             progressBar.transform.localScale = scale;
-            progressBar.transform.localPosition = new Vector3(length*ratio/2-length/2,0,2);
+            progressBar.transform.localPosition = new Vector3(length*ratio/2-length/2,0,2)+startPosition;
         }
     }
 }
