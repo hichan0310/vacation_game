b8ef9f10c7249424a335d7eb8c4d41db36759aa2
mir0173 <yoyo5260@naver.com>
2025-02-27 00:41:16 +0900

    Update lightning skill

--- Assets/GameBackend/Artifects/ElectricStone.cs
+++ Assets/GameBackend/Artifects/ElectricStone.cs
@@ -12,7 +12,7 @@ namespace GameBackend.Artifects
             if (eventArgs is NormalAttackExecuteEvent)
             for (int i = 0; i < 6; i++)
             {
-                var obj=Instantiate(ElectricSpark);
+                var obj = Instantiate(ElectricSpark);
                 obj.angle = Mathf.PI / 3 * i;
                 obj.transform.position =
                     this.player.transform.position + new Vector3(Mathf.Cos(obj.angle), Mathf.Sin(obj.angle), 0)/2;
