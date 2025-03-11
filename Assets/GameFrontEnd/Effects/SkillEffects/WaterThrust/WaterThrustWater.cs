c7f6d27e357395e1ba4771bfa98ef55b3f1075ad
mir0173 <yoyo5260@naver.com>
2025-03-12 08:27:51 +0900

    .

--- Assets/GameFrontEnd/Effects/SkillEffects/WaterThrust/WaterThrustWater.cs
+++ Assets/GameFrontEnd/Effects/SkillEffects/WaterThrust/WaterThrustWater.cs
@@ -28,7 +28,7 @@ namespace GameFrontEnd.Effects.SkillEffects.WaterThrust
 
         private HashSet<Enemy> hit=new ();
 
-        private void OnTriggerEnter(Collider other)
+        protected override void OnTriggerEnter2D(Collider2D other)
         {
             Debug.Log("dmg");
             var col = other.gameObject.GetComponent<Enemy>();
