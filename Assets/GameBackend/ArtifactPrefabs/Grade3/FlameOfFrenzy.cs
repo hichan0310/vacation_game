d89a8f6a4354aa33a0204c7e2c90df32057afce0
mir0173 <yoyo5260@naver.com>
2025-03-26 18:06:36 +0900

    Update artifact

--- Assets/GameBackend/ArtifactPrefabs/Grade3/FlameOfFrenzy.cs
+++ Assets/GameBackend/ArtifactPrefabs/Grade3/FlameOfFrenzy.cs
@@ -4,12 +4,14 @@ using GameBackend.Events;
 using UnityEngine;
 using GameBackend.Buffs;
 
-namespace GameBackend.ArtifactPrefabs.Grade_Legend
+namespace GameBackend.ArtifactPrefabs.Grade3
 {
     public class FlameOfFrenzy:Artifact, IBuffStatus
     {
         private int count = 0;
+        private int max_count = 50;
         private float duration = 0;
+        private float max_duration = 5;
         private void Start()
         {
             this.name = "광란의 불꽃";
@@ -17,7 +19,7 @@ namespace GameBackend.ArtifactPrefabs.Grade_Legend
         }
         public override void eventActive<T>(T eventArgs)
         {
-            if(eventArgs is DmgGiveEvent eve && eve.target != player && count < 50 && duration > 0.01) // 마지막조건은 여러애 동시에 맞으면 너무 빨리 오르는거 아닌가 싶어서 넣은거라 빼도됨
+            if(eventArgs is DmgGiveEvent eve && eve.target != player && count < max_count && duration > 0.01) // 마지막조건은 여러애 동시에 맞으면 너무 빨리 오르는거 아닌가 싶어서 넣은거라 빼도됨
             {
                 duration = 0;
                 count += 1;
@@ -26,7 +28,7 @@ namespace GameBackend.ArtifactPrefabs.Grade_Legend
         public override void update(float deltaTime)
         {
             duration += deltaTime;
-            if (duration > 5)
+            if (duration > max_duration)
             {
                 count = 0;
             }
